2afa714f70e5bc7f565bf26c3593377783bd5b5b
camtmccurdy <camtmccurdy@gmail.com>
2024-09-11 14:09:36 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package edu.psu.ist261;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Success");
+                new GUICalc().setVisible(true);
+	}
+
+}
